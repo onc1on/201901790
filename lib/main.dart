92f57da765a1dbe2ac3e304edd214c433fad2902
jjoh <jjoh@hufs.ac.kr>
2023-11-01 09:58:44 +0900

    파일 분리 및 HomeWidget 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,36 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_analog_clock/flutter_analog_clock.dart';
+import 'Screens/test_widget.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Home'),
+          title: const Text('Home'),
         ),
-        body: TestWidget(),
+        body: const HomeWidget(),
       ),
     ),
   );
 }
 
-class TestWidget extends StatefulWidget {
-  const TestWidget({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
 
   @override
-  State<TestWidget> createState() => _TestWidgetState();
+  State<HomeWidget> createState() => _HomeWidgetState();
 }
 
-class _TestWidgetState extends State<TestWidget> {
-  final GlobalKey<AnalogClockState> _analogClockKey = GlobalKey();
-
-  late Timer timer;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      onTick(timer);
-    });
-  }
-  
-
-
+class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text(
-            "Hello, flutter",
-            style: TextStyle(fontSize: 32, color: Colors.black),
-          ),
-          Text(
-            "Hello, student",
-            style: TextStyle(fontSize: 32, color: Colors.black),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Hello, 1",
-                style: TextStyle(fontSize: 32, color: Colors.black),
-              ),
-              Text(
-                "Hello, 2",
-                style: TextStyle(fontSize: 32, color: Colors.black),
-              ),
-              Column(
-                children: [
-                  Text(
-                    "Hello, 3",
-                    style: TextStyle(fontSize: 32, color: Colors.black),
-                  ),
-                  Text(
-                    "Hello, 4",
-                    style: TextStyle(fontSize: 32, color: Colors.black),
-                  ),
-                  Text(
-                    "Hello, 5",
-                    style: TextStyle(fontSize: 32, color: Colors.black),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Text(
-            DateTime.now().toString(),
-            style: TextStyle(fontSize: 32, color: Colors.black),
-          ),
-          Stack(
-            children: [
-              Container(
-                width: 250,
-                height: 250,
-                color: Colors.green,
-              ),
-              Container(
-                transform: Matrix4.rotationZ(0.1),
-                width: 220,
-                height: 220,
-                color: Colors.amberAccent,
-              ),
-              Container(
-                transform: Matrix4.rotationZ(0.4),
-                width: 200,
-                height: 200,
-                color: Colors.red,
-                child: AnalogClock(
-                  key: _analogClockKey,
-                  dialColor: null,
-                  markingColor: Colors.teal,
-                  hourNumberColor: Colors.teal,
-                  secondHandColor: Colors.teal,
-                ),
-              ),
-              Text(
-                "Stack",
-                style: TextStyle(fontSize: 24, color: Colors.black),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  void onTick(Timer timer) {
-    print(DateTime.now().toString());
-    setState(() {
-      _analogClockKey.currentState!.dateTime = DateTime.now();
-    });
+    return const Placeholder();
   }
 }
+
+
+
+
 
 
 
