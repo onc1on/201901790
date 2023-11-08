d9357f497b6f3082a86f15008876476d6ad09506
jjoh <jjoh@hufs.ac.kr>
2023-11-08 10:47:45 +0900

    Radio Button Test 추가

--- lib/Screens/input_test_screen.dart
+++ lib/Screens/input_test_screen.dart
@@ -20,6 +20,8 @@ class _InputTestScreenState extends State<InputTestScreen> {
           CheckBoxTest(),
           CheckBoxTest(),
           CheckBoxTest(),
+          RadioButtonTest(),
+          RadioButtonTest(),
         ],
       ),
     );
@@ -46,9 +48,12 @@ class _CheckBoxTestState extends State<CheckBoxTest> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(value: values[0], onChanged: (value) => onChanged(0, value: value)),
-        Checkbox(value: values[1], onChanged: (value) => onChanged(1, value: value)),
-        Checkbox(value: values[2], onChanged: (value) => onChanged(2, value: value)),
+        Checkbox(
+            value: values[0], onChanged: (value) => onChanged(0, value: value)),
+        Checkbox(
+            value: values[1], onChanged: (value) => onChanged(1, value: value)),
+        Checkbox(
+            value: values[2], onChanged: (value) => onChanged(2, value: value)),
       ],
     );
   }
@@ -57,5 +62,48 @@ class _CheckBoxTestState extends State<CheckBoxTest> {
     setState(() {
       values[index] = value!;
     });
+  }
+}
+
+enum TestValues {
+  R1,
+  R2,
+  R3,
+}
+
+class RadioButtonTest extends StatefulWidget {
+  const RadioButtonTest({super.key});
+
+  @override
+  State<RadioButtonTest> createState() => _RadioButtonTestState();
+}
+
+class _RadioButtonTestState extends State<RadioButtonTest> {
+  TestValues? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Radio<TestValues>(
+            value: TestValues.R1,
+            groupValue: selectedValue,
+            onChanged: (value) => setState(() {
+              selectedValue = value;
+            })),
+        Radio<TestValues>(
+            value: TestValues.R2,
+            groupValue: selectedValue,
+            onChanged: (value) => setState(() {
+              selectedValue = value;
+            })),
+        Radio<TestValues>(
+            value: TestValues.R3,
+            groupValue: selectedValue,
+            onChanged: (value) => setState(() {
+              selectedValue = value;
+            })),
+      ],
+    );
   }
 }
