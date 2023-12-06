b777e81ad6f173fa553ee5f33f7616253dcec660
jjoh <jjoh@hufs.ac.kr>
2023-12-06 09:27:49 +0900

    connect firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_project/Screens/nav_test_screen.dart';
 import 'package:mobile_project/Screens/test_widget_screen.dart';
 import 'Screens/test_widget.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart'
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  )
   dotenv.load(fileName: ".env");
 
   runApp(
